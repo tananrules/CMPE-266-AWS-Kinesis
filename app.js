86694b75f6300b00b6786074b9bf2612f1ca7b99
Tarun Arora <tarunarora0807@gmail.com>
2019-05-01 01:38:37 -0700

    languages added for translate

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const AWS = require('aws-sdk');
+const fireHose = new AWS.Firehose();
+const PubNub = require('pubnub');
+const striptags = require('striptags');
+
+
 AWS.config.update({ region: 'us-west-2' });
 
-const fireHose = new AWS.Firehose();
-
-const PubNub = require('pubnub');
+const supportedLanguages = {
+    ar: "Arabic",
+    zh: "Chinese",
+    cs: "Czech",
+    da: "Danish",
+    nl: "Dutch",
+    en: "English",
+    fi: "Finnish",
+    fr: "French",
+    de: "German",
+    he: "Hebrew",
+    id: "Indonesian",
+    it: "Italian",
+    ja: "Japanese",
+    ko: "Korean",
+    pt: "Portuguese",
+    pl: "Polish",
+    ru: "Russian",
+    es: "Spanish",
+    sv: "Swedish",
+    tr: "Turkish"
+};
 
 const pubnub = new PubNub({
     subscribe_key: 'sub-c-78806dd4-42a6-11e4-aed8-02ee2ddab7fe'
@@ -15,27 +39,30 @@ pubnub.subscribe({
 
 pubnub.addListener({
     message: function (response) {
-        let tweet = {
-            id: response.message.id,
-            text: response.message.text,
-            source: response.message.source,
-            user_screen_name: response.message.user.screen_name,
-            place: response.message.place ? response.message.place.full_name : null,
-            hashtags: response.message.entities.hashtags,
-            coordinates: response.message.coordinates,
-        };
+        
+        if(typeof supportedLanguages[response.message.lang] !== "undefined") {
+            let tweet = {
+                id: response.message.id,
+                lang: response.lang,
+                text: response.message.text,
+                source: striptags(response.message.source),
+                user_screen_name: response.message.user.screen_name,
+                place: response.message.place ? response.message.place.full_name : null,
+                hashtags: response.message.entities.hashtags,
+                coordinates: response.message.coordinates,
+            };
 
-        // console.log(tweet);
+            // console.log(tweet);
 
-        const params = {
-            DeliveryStreamName: 'pubnub-twitter-stream',
-            Record: { Data: new Buffer(JSON.stringify(tweet)) }
+            const params = {
+                DeliveryStreamName: 'pubnub-twitter-stream',
+                Record: { Data: new Buffer(JSON.stringify(tweet)) }
+            }
+
+            fireHose.putRecord(params, (err, tweet) => {
+                console.log(err, tweet);
+            });
         }
-
-        fireHose.putRecord(params, (err, tweet) => {
-            console.log(err, tweet);
-        });
-
     }
 });
 
