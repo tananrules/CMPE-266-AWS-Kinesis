12fa43e68a1b6673ed330af606e933d681413bae
Tarun Arora <tarunarora0807@gmail.com>
2019-05-01 19:05:28 -0700

    new stream added

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ pubnub.addListener({
             // console.log(tweet);
 
             const params = {
-                DeliveryStreamName: 'pubnub-twitter-raw-stream',
+                DeliveryStreamName: 'pubnub-twitter-glue-stream',
                 Record: { Data: new Buffer(JSON.stringify(tweet)) }
             }
 
